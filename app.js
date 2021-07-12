95c23ea5fc38084fdfbabd02eafe2d71fb483067
PraveenSpartacus <42913753+PraveenSpartacus@users.noreply.github.com>
2021-07-12 12:37:37 +0530

    1-added milli second sync

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-setInterval(()=>{
-    document.getElementById('message').innerText = ((new Date()).getTime().toString().slice(-6,-3));
-},1000);
+setTimeout(()=>{
+    setInterval(()=>{
+        document.getElementById('message').innerText = ((new Date()).getTime().toString().slice(-6,-3));
+    },1000);
+},(new Date()).getTime()%1000);
