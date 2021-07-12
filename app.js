ea49801fff7f7f952e99faec3e96401aaa5857ee
PraveenSpartacus <42913753+PraveenSpartacus@users.noreply.github.com>
2021-07-12 16:37:03 +0530

    3-added milli second sync

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var message = document.getElementById('message');
-setTimeout(()=>{
-    setInterval(()=>{
-        message.innerText = ((new Date()).getTime().toString());
-    },1000);
-},(new Date()).getTime()%1000);
+document.addEventListener('load',()=>{
+    var message = document.getElementById('message');
+    setTimeout(()=>{
+        setInterval(()=>{
+            message.innerText = ((new Date()).getTime().toString());
+        },1000);
+    },(new Date()).getTime()%1000);
+});
